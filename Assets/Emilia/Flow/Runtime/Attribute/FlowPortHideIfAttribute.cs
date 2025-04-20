72a11b577a5fba72edc324cfbdc78e32bb1997c5
EMT <1076995595@qq.com>
2025-04-20 23:03:02 +0800

    更新并迭代端口

---
+++ Assets/Emilia/Flow/Runtime/Attribute/FlowPortHideIfAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Emilia.Flow.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
+    public class FlowPortHideIfAttribute : Attribute
+    {
+        public string portId;
+        public object value;
+
+        public FlowPortHideIfAttribute(string portId, object value = null)
+        {
+            this.portId = portId;
+            this.value = value;
+        }
+    }
+}
---
+++ Assets/Emilia/Flow/Runtime/Attribute/FlowPortShowIfAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Emilia.Flow.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
+    public class FlowPortShowIfAttribute : Attribute
+    {
+        public string portId;
+        public object value;
+
+        public FlowPortShowIfAttribute(string portId, object value = null)
+        {
+            this.portId = portId;
+            this.value = value;
+        }
+    }
+}

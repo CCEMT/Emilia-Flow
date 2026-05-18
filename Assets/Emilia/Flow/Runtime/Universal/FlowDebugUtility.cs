cf736c56c3aa5af258755d43065c47ad92704f92
EMT <1076995595@qq.com>
2026-05-18 15:14:49 +0800

    Debug性能优化

--- Assets/Emilia/Flow/Runtime/Universal/FlowDebugUtility.cs
+++ Assets/Emilia/Flow/Runtime/Universal/FlowDebugUtility.cs
@@ -1,20 +1,44 @@
-﻿using System.Reflection;
+﻿#if UNITY_EDITOR
+using System;
+using System.Reflection;
+using Emilia.Kit.Editor;
+#endif
 
 namespace Emilia.Flow.Emilia
 {
     public static class FlowDebugUtility
     {
+#if UNITY_EDITOR
+        private static Type _editorDebugUtilityType;
+        private static bool _editorDebugUtilityResolved;
+
+        private static Type editorDebugUtilityType
+        {
+            get
+            {
+                if (_editorDebugUtilityResolved) return _editorDebugUtilityType;
+
+                _editorDebugUtilityResolved = true;
+                _editorDebugUtilityType = Assembly.Load("Emilia.Editor")?.GetType("Emilia.Flow.Editor.EditorFlowDebugUtility");
+
+                return _editorDebugUtilityType;
+            }
+        }
+#endif
+
         public static void SetState(FlowNode node, bool isDebug)
         {
 #if UNITY_EDITOR
-            Assembly.Load("Emilia.Flow.Editor").GetType("Emilia.Flow.Editor.EditorFlowDebugUtility").GetMethod("SetState").Invoke(null, new object[] {node, isDebug});
+            if (node == null) return;
+            ReflectUtility.Invoke(null, editorDebugUtilityType, nameof(SetState), new object[] {node, isDebug});
 #endif
         }
 
         public static void Ping(FlowNode node, string message)
         {
 #if UNITY_EDITOR
-            Assembly.Load("Emilia.Flow.Editor").GetType("Emilia.Flow.Editor.EditorFlowDebugUtility").GetMethod("Ping").Invoke(null, new object[] {node, message});
+            if (node == null) return;
+            ReflectUtility.Invoke(null, editorDebugUtilityType, nameof(Ping), new object[] {node, message});
 #endif
         }
     }
